05f1ae688dd57fa5d1e8981624acc894b8623534
Jamie Mason <siunmaru@gmail.com>
2012-02-19 22:11:19 +0000

    Renamed files in test suite

---
+++ test/spec.js
@@ -0,0 +1,102 @@
+/*global describe,it,expect,peach*/
+
+(function()
+{
+  describe("peach", function()
+  {
+    it("provides the value and index of each member of the collection in turn", function()
+    {
+      // precompile
+      var iterator = peach(function(val, i)
+          {
+            expect(val).toEqual(i + 1);
+          });
+
+      // run
+      iterator([1, 2, 3]);
+    });
+
+    it("loses access to any local variables found in the enclosing scope at compile time", function()
+    {
+      var someValue = "Hello",
+          iterator = peach(function(val, i, arr)
+          {
+            expect(val).toEqual(i + 1);
+            expect(typeof someValue).toEqual("undefined");
+          });
+
+      iterator([1, 2, 3]);
+    });
+
+    it("gains access to any local variables passed as arguments at run time", function()
+    {
+      var someValue = "Hello",
+          otherValue = ", is it me you're looking for?",
+          iterator = peach(function(val, i, arr, someValue, otherValue)
+          {
+            expect(val).toEqual(i + 1);
+            expect(someValue).toEqual("Hello");
+            expect(otherValue).toEqual(", is it me you're looking for?");
+          });
+
+      iterator([1, 2, 3], someValue, otherValue);
+    });
+
+    it("returns an object of the values of local variables", function()
+    {
+      var collection = [1, 2, 3],
+          iterator,
+          answer = false,
+          locals;
+
+      // compile
+      iterator = peach(function(num, index, arr, answer)
+      {
+        if (_.include(arr, num))
+        {
+          answer = true;
+        }
+      });
+
+      // run
+      locals = iterator(collection, answer);
+      expect(locals.answer).toEqual(true);
+    });
+
+    it("iterates over Objects as well as Arrays, and ignores the Object's Prototype", function()
+    {
+      var collection = {
+            one: 1,
+            two: 2,
+            three: 3
+          },
+          answers = [],
+          iterator = peach(function(val, key, obj, answers)
+          {
+            answers.push(key);
+          });
+
+      collection.constructor.prototype.four = 4;
+      iterator(collection, answers);
+      expect(answers.join(", ")).toEqual("one, two, three");
+      delete collection.constructor.prototype.four;
+    });
+
+    it("handles a null properly", function()
+    {
+      var answers = 0,
+          iterator = peach(function(val, key, obj, answers)
+          {
+            answers++;
+          }),
+          locals;
+
+      expect(function () {
+        locals = iterator(null, answers);
+      }).not.toThrowError();
+
+      expect(locals.answers).toEqual(0);
+    });
+
+  });
+}());
