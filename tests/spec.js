9a7efe61d384ab4f1cf3d21730997dcbfae0c768
Jamie Mason <siunmaru@gmail.com>
2012-03-15 23:25:25 +0000

    minor edits to test suite

--- tests/spec.js
+++ tests/spec.js
@@ -1,59 +1,47 @@
 /*global describe,it,expect,pEach*/
 
-(function()
-{
-  describe("pEach", function()
-  {
-    it("provides the value and index of each member of the collection in turn", function()
-    {
+(function() {
+  describe("pEach", function() {
+    it("provides the value and index of each member of the collection in turn", function() {
       // precompile
-      var iterator = pEach(function(val, i)
-          {
-            expect(val).toEqual(i + 1);
-          });
+      var iterator = pEach(function(val, i) {
+        expect(val).toEqual(i + 1);
+      });
 
       // run
       iterator([1, 2, 3]);
     });
 
-    it("loses access to any local variables found in the enclosing scope at compile time", function()
-    {
+    it("loses access to any local variables found in the enclosing scope at compile time", function() {
       var someValue = "Hello",
-          iterator = pEach(function(val, i, arr)
-          {
-            expect(val).toEqual(i + 1);
-            expect(typeof someValue).toEqual("undefined");
-          });
+        iterator = pEach(function(val, i, arr) {
+          expect(val).toEqual(i + 1);
+          expect(typeof someValue).toEqual("undefined");
+        });
 
       iterator([1, 2, 3]);
     });
 
-    it("gains access to any local variables passed as arguments at run time", function()
-    {
+    it("gains access to any local variables passed as arguments at run time", function() {
       var someValue = "Hello",
-          otherValue = ", is it me you're looking for?",
-          iterator = pEach(function(val, i, arr, someValue, otherValue)
-          {
-            expect(val).toEqual(i + 1);
-            expect(someValue).toEqual("Hello");
-            expect(otherValue).toEqual(", is it me you're looking for?");
-          });
+        otherValue = ", is it me you're looking for?",
+        iterator = pEach(function(val, i, arr, someValue, otherValue) {
+          expect(val).toEqual(i + 1);
+          expect(someValue).toEqual("Hello");
+          expect(otherValue).toEqual(", is it me you're looking for?");
+        });
 
       iterator([1, 2, 3], someValue, otherValue);
     });
 
-    it("returns an object of the values of local variables", function()
-    {
+    it("returns an object of the values of local variables", function() {
       var collection = [1, 2, 3],
-          iterator,
-          answer = false,
-          locals;
+        iterator, answer = false,
+        locals;
 
       // compile
-      iterator = pEach(function(num, index, arr, answer)
-      {
-        if (num === 2)
-        {
+      iterator = pEach(function(num, index, arr, answer) {
+        if (num === 2) {
           answer = true;
         }
       });
@@ -63,18 +51,16 @@
       expect(locals.answer).toEqual(true);
     });
 
-    it("iterates over Objects as well as Arrays, and ignores the Object's Prototype", function()
-    {
+    it("iterates over Objects as well as Arrays, and ignores the Object's Prototype", function() {
       var collection = {
-            one: 1,
-            two: 2,
-            three: 3
-          },
-          answers = [],
-          iterator = pEach(function(val, key, obj, answers)
-          {
-            answers.push(key);
-          });
+        one: 1,
+        two: 2,
+        three: 3
+      },
+        answers = [],
+        iterator = pEach(function(val, key, obj, answers) {
+          answers.push(key);
+        });
 
       collection.constructor.prototype.four = 4;
       iterator(collection, answers);
@@ -82,16 +68,14 @@
       delete collection.constructor.prototype.four;
     });
 
-    it("handles a null properly", function()
-    {
+    it("handles a null properly", function() {
       var answers = 0,
-          iterator = pEach(function(val, key, obj, answers)
-          {
-            answers++;
-          }),
-          locals;
+        iterator = pEach(function(val, key, obj, answers) {
+          answers++;
+        }),
+        locals;
 
-      expect(function () {
+      expect(function() {
         locals = iterator(null, answers);
       }).not.toThrowError();
 
